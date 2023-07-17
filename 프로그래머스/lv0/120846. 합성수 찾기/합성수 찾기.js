f6a2e1476d01725a4e310b246ac32a8489075b8e
wonsik <121328277+Choiwonsikk@users.noreply.github.com>
2023-07-18 02:10:50 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = 0;
+    for (i = 4; i <= n; i ++) {
+        for (j = 2; j < i; j ++) {
+            if(i % j == 0) {
+                answer += 1;
+                break
+            }
+        }
+    } return answer;
+}
