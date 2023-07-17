be9f0c7cbe85d1e504a0df05332273fe6a19e4f7
wonsik <121328277+Choiwonsikk@users.noreply.github.com>
2023-07-18 01:48:21 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,3 +1,7 @@
 function solution(number) {
-    return [...number].reduce((acc, cur) => acc+Number(cur), 0)%9
+    let answer = 0
+    for(let i=0; i<number.length; i++){
+        answer += Number(number[i])
+    }
+    return answer%9
 }
