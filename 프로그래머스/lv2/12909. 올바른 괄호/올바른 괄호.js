a1eab1c53d095c60a14d459d876ccf7766cff518
wonsik <121328277+Choiwonsikk@users.noreply.github.com>
2023-08-07 22:17:13 +0900

    [level 2] Title: 올바른 괄호, Time: 8.09 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,11 +1,12 @@
 function solution(s){
     let arr = []
-    s.split("").map( a => {
+    for(a of s.split(""))
+    {    
         if(a === ")" && arr[arr.length-1] === "("){
             arr.pop()
         }else{
         arr.push(a)
         }
-    }) 
+    }
     return arr.length === 0
 }
