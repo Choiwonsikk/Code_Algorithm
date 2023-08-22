bbc9eeb25adebb731f6fbc2fe03596f3e2327a47
wonsik <121328277+Choiwonsikk@users.noreply.github.com>
2023-08-22 22:36:01 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,11 +1,4 @@
 function solution(box, n) {
-    let result = 0;
-    for(let i=0; i<box.length; i++){
-        if(i === 0){
-            result += parseInt(box[i]/n)
-            continue
-        }
-        result *= parseInt(box[i]/n)
-    }
-    return result
+    let [width, length, height] = box
+    return parseInt(width/n) * parseInt(length/n) * parseInt(height/n)
 }
