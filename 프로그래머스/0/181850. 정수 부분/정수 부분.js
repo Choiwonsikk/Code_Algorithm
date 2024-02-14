6c68e8e3607f215b8aaddc65fba8f743a9786baa
wonsik <121328277+Choiwonsikk@users.noreply.github.com>
2024-02-14 15:03:24 +0900

    [level 0] Title: 정수 부분, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.js
@@ -0,0 +1,8 @@
+function solution(flo) {
+    return Math.trunc(flo)
+/*Math의 다른 함수 3개 Math.floor(), Math.ceil() 그리고 Math.round() 와는 다르게, Math.trunc() 단순하게 동작합니다. 함수는 주어진 값이 양수이건 음수이건 상관없이 소수점 이하 우측부분을 제거하는 매우 단순한 동작을 합니다.
+
+함수인자는 암묵적으로 숫자로 변환되어 메서드에 전달됩니다.
+
+trunc() 함수는 Math의 정적 메서드이기 때문에 사용자가 생성한 Math 객체의 메서드로 호출하는 것이 아닌 항상 Math.trunc() 형태로 호출해야 합니다.*/
+}
